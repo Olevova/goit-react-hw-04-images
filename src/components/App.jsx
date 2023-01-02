ac5b83d306a781d88ad20db2a376e93b72ec55d7
Olevova <olevova1983@gmail.com>
2023-01-02 10:37:09 +0200

    all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,7 @@ import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Spiner } from './Loader/Loader';
 import { pictureResp } from '../HelpComp/Respons';
 import { MySwal } from '../HelpComp/Answer';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
   const [gallery, setGallery] = useState([]);
@@ -18,15 +17,6 @@ export const App = () => {
   const [bigImage, setBigImage] = useState(false);
   const [totalHitsLeft, setTotalHitsLeft] = useState(null);
   const [downmore, setDownload] = useState(false);
-  //   state = {
-  //   gallery: null,
-  //   page: 0,
-  //   search: null,
-  //   isdownload:null,
-  //   isload: null,
-  //   bigImage: false
-
-  // }
 
   const onHandler = props => {
     console.log(props);
@@ -37,26 +27,25 @@ export const App = () => {
       });
       return;
     }
-    setGallery(null);
+
+    setGallery([]);
     setPage(0);
     setIsload(null);
     setBigImage(false);
     setSearch(props);
     setIsdownload(true);
     setTotalHitsLeft(null);
+    setDownload(false);
   };
 
   useEffect(() => {
     if (!search) {
       return;
     }
-    console.log(search);
     async function myResponse() {
       if (!downmore) {
-        console.log(13);
         setIsdownload(true);
         const respons = await pictureResp(search, page);
-        console.log(respons.data, 57);
         const {
           data: { totalHits, hits },
         } = respons;
@@ -68,17 +57,15 @@ export const App = () => {
           });
         }
 
-        if (search) {
+        if (page < 1) {
           setIsload(true);
           setIsdownload(null);
           setGallery(hits);
-          console.log(74);
           setTotalHitsLeft(totalHits - 12);
           setDownload(true);
           return;
         }
 
-        console.log(83);
         setGallery(prevImage => [...prevImage, ...hits]);
         setTotalHitsLeft(totalHitsLeft - 12);
         setIsdownload(null);
@@ -86,7 +73,7 @@ export const App = () => {
       }
     }
     myResponse();
-  }, [search, page, isdownload, isload, totalHitsLeft, gallery]);
+  }, [search, page, isdownload, isload, totalHitsLeft, gallery, downmore]);
 
   const onLargePicture = e => {
     setBigImage(e);
@@ -115,101 +102,3 @@ export const App = () => {
     </div>
   );
 };
-
-// export class App extends Component {
-
-//   state = {
-//   gallery: null,
-//   page: 0,
-//   search: null,
-//   isdownload:null,
-//   isload: null,
-//   bigImage: false
-
-// }
-
-//   onHandler = (props) => {
-//     const {search} = this.state;
-//     console.log(props);
-//     if (search === props) {
-//       MySwal.fire({
-//         title: "try new word",
-//         icon: "warning"
-//       })
-//       return
-//     }
-
-//     this.setState({
-//       gallery:null,
-//       page: 0,
-//       isload: null,
-//       bigImage: false,
-//       search: props,
-//       isdownload: true,
-//       totalHitsLeft: null
-//   })
-//   }
-
-// async componentDidUpdate(prevProps, prevState) {
-
-//   const {page, search} = this.state
-
-//   if (prevState.search !== search || prevState.page !== page) {
-//     console.log(prevState.page, page);
-//     this.setState({isdownload:true});
-//     const respons = await pictureResp(search, page);
-//     const {data:{totalHits,hits}} = respons;
-//     if (hits.length < 1) {
-//       MySwal.fire({
-//         title: "Nothing found on this request",
-//         icon: "warning"
-//       })
-//     }
-
-//     if (prevState.search !== search) {
-//       this.setState({
-//         isload: true,
-//         isdownload: null,
-//         gallery: hits,
-//         totalHitsLeft: totalHits-12
-//       })
-//       return
-//     }
-//     console.log(respons.data);
-//     this.setState((prev) => ({
-//       gallery: [...prev.gallery, ...hits],
-//       totalHitsLeft: prev.totalHitsLeft-12,
-//       isdownload:null
-//           })
-//       )
-//   }
-//   }
-
-// onLargePicture=(e)=> {
-//   this.setState({bigImage : e})
-//   }
-
-// onModalClick = (e) => {
-//   this.setState((prev)=>({bigImage : !prev.bigImage}))
-//   }
-
-// onClickLoad = () => {
-//     this.setState((prev) =>
-//       ({page: prev.page+1}))
-//   }
-
-// render(){
-
-//   const {gallery, page, isload, isdownload, totalHitsLeft,bigImage} = this.state
-//   return <div>
-
-//     <Searchbar onSubmit={this.onHandler} />
-//     {bigImage &&
-//     <Modal bigImage={bigImage} onModalClick={this.onModalClick } />
-//     }
-//     {isload && <ImageGallery onRender={gallery} onLargePicture={this.onLargePicture} />}
-//     {isdownload && <Spiner/>}
-//     {page >= 0 && totalHitsLeft>0 &&<ButtonLoad onClickSearch={this.onClickLoad} />}
-//      </div>
-//   };
-// };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,53 +1,46 @@
-import { Component } from "react";
 import { ImRocket } from 'react-icons/im';
-import style from "./Searchbar.module.scss"
-import PropTypes from "prop-types";
+import style from './Searchbar.module.scss';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
+export const Searchbar = ({ onSubmit }) => {
+  const [picture, setPicture] = useState('');
 
+  const onChangePicture = e => {
+    setPicture(e.currentTarget.value.toLowerCase());
+  };
 
-export class Searchbar extends Component{
-    state = {
-        picture:''
-  }
+  const formHandler = e => {
+    e.preventDefault();
+    console.log(e.target.picture.value);
+    onSubmit(picture);
+  };
 
-    onChangePicture = (e) => {
-        this.setState({picture:e.currentTarget.value.toLowerCase()})
-        
-    }
-    
-    formHandler = (e) => {
-        e.preventDefault();
-        console.log(e.target.picture.value);
-        this.props.onSubmit(this.state.picture)
+  return (
+    <header className={style.Searchbar}>
+      <form className={style.SearchForm} onSubmit={formHandler}>
+        <button type="submit" className={style.SearchForm_button}>
+          <ImRocket
+            style={{ display: 'flex', justifyContent: 'center', fill: 'red' }}
+          />
+          <span className={style.SearchForm_button_label}>Search</span>
+        </button>
 
-        
- }
-    
-    render() {
-        return (
-            <header className={style.Searchbar } >
-                <form className={style.SearchForm }  onSubmit={this.formHandler}>
-                    <button type="submit" className={style.SearchForm_button}>
-                        <ImRocket style={{ display: "flex", justifyContent: "center", fill: "red"}} />
-                        <span className={style.SearchForm_button_label}>Search</span>
-                    </button>
+        <input
+          className={style.SearchForm_input}
+          type="text"
+          name="picture"
+          value={picture}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={onChangePicture}
+        />
+      </form>
+    </header>
+  );
+};
 
-                    <input
-                        className={style.SearchForm_input}
-                        type="text"
-                        name="picture"
-                        value={this.state.picture}
-                        autoComplete="off"
-                        autoFocus
-                        placeholder="Search images and photos"
-                        onChange={this.onChangePicture}
-                    />
-                </form>
-            </header>)
-    }
-}
-
-Searchbar.propTypes ={
-    picture: PropTypes.string,
-    
-}
+Searchbar.propTypes = {
+  picture: PropTypes.string,
+};
